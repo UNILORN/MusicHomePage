a942cf7b331bfdf8a3f23efcdb59e718ef0b521c
UNILORN <b5159@oic.jp>
2016-07-30 14:37:03 +0000

    Init Files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require __DIR__."/functions/function.php"; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Unilorn.com</title>
+  </head>
+  <body>
+
+
+  </body>
+</html>
